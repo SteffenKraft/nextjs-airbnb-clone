1243bc603137cc750d53c01bea49755e74be2859
SteffenKraft <steffen.kraft@mail.de>
2023-05-25 15:16:17 +0200

    categories, query-string

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,8 +6,10 @@ import React from "react"
 import { useRouter } from "next/navigation"
 
 const Logo = () => {
+  const router = useRouter()
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
       className="hidden cursor-pointer md:block"
       src="/images/logo.png"
