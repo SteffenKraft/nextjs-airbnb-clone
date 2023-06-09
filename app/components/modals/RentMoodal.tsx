b7532ebb462d84c6e665be858de9b0428a6f1aa2
SteffenKraft <steffen.kraft@mail.de>
2023-06-09 10:53:57 +0200

    finishing rentModal, prisma listing create

--- app/components/modals/RentMoodal.tsx
+++ app/components/modals/RentMoodal.tsx
@@ -4,12 +4,16 @@ import Heading from "../Heading"
 import Modal from "./Modal"
 import useRentModal from "@/app/hooks/useRentModal"
 import { categories } from "../navbar/Categories"
-import { FieldValues, useForm } from "react-hook-form"
+import { FieldValues, useForm, SubmitHandler } from "react-hook-form"
 import CategoryInput from "../inputs/CategoryInput"
 import CountrySelect from "../inputs/CountrySelect"
 import Counter from "../inputs/Counter"
 import ImageUpload from "../inputs/ImageUpload"
 import dynamic from "next/dynamic"
+import Input from "../inputs/Input"
+import { toast } from "react-hot-toast"
+import { useRouter } from "next/navigation"
+import axios from "axios"
 
 enum STEPS {
   CATEGORY,
@@ -22,9 +26,12 @@ enum STEPS {
 
 const RentModal = () => {
   const [step, setStep] = useState(STEPS.CATEGORY)
+  const [isLoading, setIsLoading] = useState(false)
 
   const onBack = () => setStep((value) => value - 1)
   const onNext = () => setStep((value) => value + 1)
+
+  const router = useRouter()
 
   const {
     register,
@@ -37,7 +44,7 @@ const RentModal = () => {
     defaultValues: {
       category: "",
       location: "",
-      gestCount: 1,
+      guestCount: 1,
       roomCount: 1,
       bathroomCount: 1,
       imageSrc: "",
@@ -55,9 +62,31 @@ const RentModal = () => {
     })
   }
 
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext()
+    }
+    setIsLoading(true)
+    axios
+      .post("/api/listings", data)
+      .then(() => {
+        toast.success("Listing created")
+        router.refresh()
+        reset()
+        setStep(STEPS.CATEGORY)
+        rentModal.onClose()
+      })
+      .catch(() => {
+        toast.error("Something went wrong")
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
   const category = watch("category")
   const location = watch("location")
-  const gestCount = watch("gestCount")
+  const guestCount = watch("guestCount")
   const roomCount = watch("roomCount")
   const bathroomCount = watch("bathroomCount")
   const imageSrc = watch("imageSrc")
@@ -128,8 +157,8 @@ const RentModal = () => {
         <Counter
           title="Guests"
           subtitle="How many guests do you allow?"
-          value={gestCount}
-          onChange={(value) => setCustomValue("gestCount", value)}
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
         />
         <hr />
         <Counter
@@ -164,6 +193,54 @@ const RentModal = () => {
     )
   }
 
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and Sweet works best."
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          type="number"
+          required
+        />
+      </div>
+    )
+  }
+
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
@@ -177,7 +254,7 @@ const RentModal = () => {
       title="Rent"
       actionLabel={step === STEPS.PRICE ? "Create" : "Next"}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={step === STEPS.CATEGORY ? undefined : "Back"}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
       body={bodyContent}
